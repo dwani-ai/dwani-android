2f29ffd397e8120e8b69628c995ccb14c8503c03
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-03-08 12:38:40 +0100

    fix build

--- app/src/main/java/com/slabstech/dhwani/voiceai/DhwaniApplication.kt
+++ app/src/main/java/com/slabstech/dhwani/voiceai/DhwaniApplication.kt
@@ -1,16 +1,16 @@
 package com.slabstech.dhwani.voiceai
 
 import android.app.Application
+import com.slabstech.dhwani.voiceai.di.appModule // We'll define this next
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import com.slabstech.dhwani.voiceai.di.appModule  // We'll define this next
 
 class DhwaniApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidContext(this@DhwaniApplication)  // Provides Android context to Koin
-            modules(appModule)  // Load the app module with dependencies
+            androidContext(this@DhwaniApplication) // Provides Android context to Koin
+            modules(appModule) // Load the app module with dependencies
         }
     }
 }
--- app/src/main/java/com/slabstech/dhwani/voiceai/di/NetworkModule.kt
+++ app/src/main/java/com/slabstech/dhwani/voiceai/di/NetworkModule.kt
@@ -4,12 +4,13 @@ import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import java.util.concurrent.TimeUnit
 
-val appModule = module {
-    single {
-        OkHttpClient.Builder()
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .writeTimeout(30, TimeUnit.SECONDS)
-            .build()
+val appModule =
+    module {
+        single {
+            OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build()
+        }
     }
-}

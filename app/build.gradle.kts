56f6e678aa976c6ecea4fd8c66ce7a0cb586241c
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-03-16 21:58:24 +0100

    s

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.slabstech.dhwani.voiceai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
+        versionCode = 25
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

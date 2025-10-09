16b28ae3c5330be698b6999cb9fed914490ef411
Sachin Shetty <26170834+sachinsshetty@users.noreply.github.com>
2025-10-09 13:00:07 +0200

    add-malayam (#25)
    * add-malayam
    
    * upgrade-app
    
    * fix-permission
    
    * upare-version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.slabstech.dhwani.voiceai"
         minSdk = 27
         targetSdk = 36
-        versionCode = 78
-        versionName = "1.1.2"
+        versionCode = 82
+        versionName = "1.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

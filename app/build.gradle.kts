4f5a7759f15e209be3d0e37865ad02dd8e603a1c
sachin <26170834+sachinsshetty@users.noreply.github.com>
2026-04-11 12:06:59 +0530

    add-voice-agents

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.slabstech.dhwani.voiceai"
         minSdk = 27
         targetSdk = 36
-        versionCode = 96
+        versionCode = 97
         versionName = "1.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
